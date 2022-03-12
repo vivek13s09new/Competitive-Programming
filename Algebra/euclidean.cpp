2deebd1f5078944dd2caa1995f8dea984f691fe9
vivek13s09new <71691561+vivek13s09new@users.noreply.github.com>
2022-03-12 23:34:35 +0530

    Update euclidean.cpp

--- Algebra/euclidean.cpp
+++ Algebra/euclidean.cpp
@@ -6,3 +6,23 @@ int gcd(int n1, int n2){
   }
   return n2;
 }
+
+int gcd2(int n1, int n2) {
+    if (n2 == 0)
+        return n1;
+    else
+        return gcd (n2, n1 % n2);
+}
+
+int gcd3(int n1, int n2) {
+    return n2 ? gcd (n2, n1 % n2) : n1;
+}
+
+int gcd4(int n1, int n2) {
+    while (n2) {
+        n1 %= n2;
+        swap(n1, n2);
+    }
+    return n1;
+}
+
